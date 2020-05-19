125ed6ea6242851e7d4b43c3d071d685000144bd
Charles Ross <chivalry@mac.com>
2020-05-19 15:46:05 -0700

    Chapter 3 section 5

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,5 +1,23 @@
 console.log('app.js is running')
 
-var template = <h1>Indecision App</h1>
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item One</li>
+            <li>Item Two</li>
+        </ol>
+    </div>
+);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+
+var templateTwo = (
+    <div>
+        <h1>Charles Ross</h1>
+        <p>Age: 48</p>
+        <p>Location: Hemet, CA</p>
+    </div>
+);
+ReactDOM.render(templateTwo, appRoot);
