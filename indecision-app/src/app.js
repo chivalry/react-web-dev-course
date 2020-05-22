b913934cb2ad4f9e4e8f93a692b2723c5156552e
Charles Ross <chivalry@mac.com>
2020-05-22 11:27:33 -0700

    Chapter 3 section 12

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -17,25 +17,32 @@ const appRoot = document.getElementById('app');
 let count = 0;
 
 const addOne = () => {
-    console.log('addOne');
+    count++;
+    renderCounterApp();
 }
 
 const minusOne = () => {
-    console.log('minusOne');
+    count--;
+    renderCounterApp();
 }
 
 const reset = () => {
-    console.log('reset');
+    count = 0;
+    renderCounterApp();
 }
 
 const someId = 'myidehere';
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-);
 
-ReactDOM.render(templateTwo, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, appRoot);  
+}
+
+renderCounterApp();
