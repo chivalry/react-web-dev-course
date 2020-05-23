3f97fd79d76221f104f28c6c6dfd8a56416ffe48
Charles Ross <chivalry@mac.com>
2020-05-22 18:46:49 -0700

    Chapter 4 section 7

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,10 +1,13 @@
 class IndecisionApp extends React.Component {
     render () {
+        const title = 'Indecision';
+        const subtitle = 'Put your life in the hands of a computer';
+        const options = ['thing one', 'thing two', 'thing four'];
         return (
             <div>
-                <Header />
+                <Header title={title} subtitle={subtitle}/>
                 <Action />
-                <Options />
+                <Options options={options}/>
                 <AddOption />
             </div>
         )
@@ -14,8 +17,8 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
             </div>
         );
     }
@@ -35,8 +38,7 @@ class Options extends React.Component {
     render() {
         return (
             <div>
-                <p>Options component here</p>
-                <Option />
+                {this.props.options.map((option) => <Option key={option} optionText={option} />)}
             </div>
         );
     }
@@ -46,7 +48,7 @@ class Option extends React.Component {
     render() {
         return (
             <div>
-                <p>Option Component here</p>
+                {this.props.optionText}
             </div>
         )
     }
