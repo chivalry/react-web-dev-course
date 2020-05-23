728a4692d61df40e55cde8a9be398935c4938ec2
Charles Ross <chivalry@mac.com>
2020-05-23 13:54:01 -0700

    chapter 4 section 6

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -9,6 +9,15 @@ class IndecisionApp extends React.Component {
             options: props.options
         };
     }
+    componentDidMount() {
+        console.log('fetching data');
+    }
+    componentDidUpdate(prevProps, prevState) {
+        console.log('saving data');
+    }
+    componentWillUnmount() {
+        console.log('unmount');
+    }
     handleDeleteOptions() {
         this.setState(() => ({options: []}));
     }
