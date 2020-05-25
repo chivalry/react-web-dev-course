5702407bc4fcac82309e64439b7a188e7ceb7ca8
Charles Ross <chivalry@mac.com>
2020-05-25 13:56:23 -0700

    React Route section 9

---
+++ portfolio-site/src/components/ContactPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AddExpensePage = () => (
+    <div>
+        Contact me at <a href="mailto:chivalry@mac.com">chivalry@mac.com</a>
+    </div>
+);
+
+export default AddExpensePage;
