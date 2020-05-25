6e9a7f1e5e636d5e39061548db7a6680c03970eb
Charles Ross <chivalry@mac.com>
2020-05-25 13:21:01 -0700

    React Route section 8

--- expensify-app/src/components/EditExpensePage.js
+++ expensify-app/src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>
-        This is the edit page
-    </div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Editing expense {props.match.params.id}
+        </div>
+    );
+};
 
 export default EditExpensePage;
