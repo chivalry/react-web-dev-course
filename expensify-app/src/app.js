f087a1699fad639ab9ff74466777d5a356524c64
Charles Ross <chivalry@mac.com>
2020-05-26 14:50:19 -0700

    React Redux section 2

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
+store.subscribe(() => {
+    console.log(store.getState());
+});
+store.dispatch(addExpense({description: 'Water Bill'}));
+store.dispatch(addExpense({description: 'Gas bill'}));
+store.dispatch(setTextFilter('bill'));
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
+store.dispatch(setTextFilter('water'));
+console.log(getVisibleExpenses(store.getState().expenses, store.getState().filters));
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
