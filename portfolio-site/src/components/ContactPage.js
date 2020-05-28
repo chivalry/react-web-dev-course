129b3b676186634497c4a14b42fad989214d82d3
Charles Ross <chivalry@mac.com>
2020-05-28 13:50:19 -0700

    Final app

--- portfolio-site/src/components/ContactPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddExpensePage = () => (
-    <div>
-        Contact me at <a href="mailto:chivalry@mac.com">chivalry@mac.com</a>
-    </div>
-);
-
-export default AddExpensePage;
